72c7adf63e1675c4f1082e72347f0e5f437cc4bb
wallyson134 <86493666+wallyson134@users.noreply.github.com>
2022-11-25 00:17:23 -0300

    Projeto Finzalizado em parte

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import { Lesson } from "./Lesson";
+import { gql, useQuery } from "@apollo/client"
+import {useGetLessonsQuery} from '../graphql/generated'
+
+
+
+export function Sidebar() {
+  const { data } = useGetLessonsQuery()
+
+  console.log(data)
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas
+      </span>
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map(lessons => {
+          return (
+            <Lesson
+              key={lessons.id}
+              title={lessons.title}
+              slug={lessons.slug}
+              availableAt={new Date(lessons.availableAt)}
+              type={lessons.lessonType}
+            />
+          )
+        })}
+
+      </div>
+    </aside>
+
+  )
+}
